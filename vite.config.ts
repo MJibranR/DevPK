aacf5b5ed0f268960ebd3e6af40cc82961cda433
MJibranR <muhammadjibranrehan28@gmail.com>
2026-04-17 21:47:14 +0500

    Commited

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     build: {
-      ssr: false,
       outDir: "dist/client"
     }
   }
