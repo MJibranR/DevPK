19bd326e0332b02dc6779b994505490a3b27f17e
MJibranR <muhammadjibranrehan28@gmail.com>
2026-04-17 22:02:26 +0500

    Done

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     build: {
-      outDir: "dist",
+      outDir: "dist/client",
+      emptyOutDir: true
     }
   }
 });
